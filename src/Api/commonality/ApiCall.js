1ca9e7f15f7d1ceb3ec5f78428133de59eccc975
lqh <1040152089@qq.com>
2018-06-08 15:09:16 +0800

    清理bug

--- src/Api/commonality/ApiCall.js
+++ src/Api/commonality/ApiCall.js
@@ -17,13 +17,13 @@ function apiCall (parm, URL) {
         }
     }
     // 内网
-    // let serverHost = process.env.NODE_ENV === 'development' ? 'http://192.168.100.110:8083/yunzhubao' : ''
+    let serverHost = process.env.NODE_ENV === 'development' ? 'http://192.168.100.110:8083/yunzhubao' : ''
     // uat
     // let serverHost = process.env.NODE_ENV === 'development' ? 'http://192.168.100.110:8099' : ''
     // 正式
     // let serverHost = process.env.NODE_ENV === 'development' ? 'https://www.yunzhubao.com' : ''
     // 本地
-    let serverHost = process.env.NODE_ENV === 'development' ? 'http://192.168.100.109:8080/yunzhubao' : ''
+    // let serverHost = process.env.NODE_ENV === 'development' ? 'http://192.168.100.109:8080/yunzhubao' : ''
     let url = serverHost + URL
     //console.log(url)
     let foo = function* (data) {
@@ -33,14 +33,14 @@ function apiCall (parm, URL) {
     
     let resourcePromise = foo(data).next().value
     // 中间件
-    resourcePromise.then( res =>{
-        // console.log('中间件拦截', res)
-      // 截获登录超时
-      if(res.data.state == 100 ){
-        Vue.prototype.loginPopup.show()
-      }
+    // resourcePromise.then( res =>{
+    //     // console.log('中间件拦截', res)
+    //   // 截获登录超时
+    //   if(res.data.state == 100 ){
+    //     Vue.prototype.loginPopup.show()
+    //   }
       
-    })
+    // })
     
     return resourcePromise
 }
