d899bb2d653bda36810447e271b2ea65ac9b7499
john <1351009316@qq.com>
2018-06-27 11:40:17 +0800

    调整表格

--- src/components/jcp-print/sell/cash-template.vue
+++ src/components/jcp-print/sell/cash-template.vue
@@ -20,50 +20,50 @@
         </tr> -->
         <tr>
           <!-- <td>序号</td> -->
-          <td>销售类型</td>
-          <td>回购类型</td>
-          <td>产品类别</td>
-          <td>件数(件)</td>
-          <td>件重(g)</td>
-          <td>金重(g)</td>
-          <td>标价(元)</td>
-          <td>工费总额(元)</td>
-          <td>实售价(元)</td>
+          <td class="tm">销售类型</td>
+          <td class="tm">回购类型</td>
+          <td class="tm">产品类别</td>
+          <td class="tm">件数(件)</td>
+          <td class="tm">件重(g)</td>
+          <td class="tm">金重(g)</td>
+          <td class="tm">标价(元)</td>
+          <td class="tm">工费总额(元)</td>
+          <td class="tm">实售价(元)</td>
         </tr>
         <template v-if="sellStorage.productTypeList" v-for="(productTypeList,j) in sellStorage.productTypeList">
           <template v-if="productTypeList.productSellTypeList" v-for="(productSellTypeList,i) in productTypeList.productSellTypeList">
 
             <tr v-for="(item,index) in productSellTypeList.productTypeList" :key="index">
-              <td v-if="j == 0 && index == 0 && i == 0" :rowspan="sellLength(sellStorage.productTypeList)">销售</td>
-              <td v-if="index == 0 && productSellTypeList.classesType == '1'" :rowspan="productSellTypeList.productTypeList.length">计重</td>
-              <td v-else-if="index == 0 && productSellTypeList.classesType == '2'" :rowspan="productSellTypeList.productTypeList.length">计件</td>
+              <td class="tm" v-if="j == 0 && index == 0 && i == 0" :rowspan="sellLength(sellStorage.productTypeList)">销售</td>
+              <td class="tm" v-if="index == 0 && productSellTypeList.classesType == '1'" :rowspan="productSellTypeList.productTypeList.length">计重</td>
+              <td class="tm" v-else-if="index == 0 && productSellTypeList.classesType == '2'" :rowspan="productSellTypeList.productTypeList.length">计件</td>
               <!-- <td>{{index+1}}</td> -->
-              <td>{{item.className}}</td>
-              <td>{{item.totalNum}}</td>
-              <td>{{item.totalWeight}}</td>
-              <td>{{item.totalGoldWeight}}</td>
-              <td>{{item.totalSoldPrice}}</td>
-              <td>{{item.totalWage}}</td>
-              <td>{{item.totalRealPrice}}</td>
+              <td class="tl">{{item.className}}</td>
+              <td class="tr">{{item.totalNum}}</td>
+              <td class="tr">{{item.totalWeight}}</td>
+              <td class="tr">{{item.totalGoldWeight}}</td>
+              <td class="tr">{{item.totalSoldPrice}}</td>
+              <td class="tr">{{item.totalWage}}</td>
+              <td class="tr">{{item.totalRealPrice}}</td>
             </tr>
             <tr>
-              <td colspan="2">小计</td>
-              <td>{{productSellTypeList.totalNum}}</td>
-              <td>{{productSellTypeList.totalWeight}}</td>
-              <td>{{productSellTypeList.totalGoldWeight}}</td>
-              <td>{{productSellTypeList.totalSoldPrice}}</td>
-              <td>{{productSellTypeList.totalWage}}</td>
-              <td>{{productSellTypeList.totalRealPrice}}</td>
+              <td class="tm" colspan="2">小计</td>
+              <td class="tr">{{productSellTypeList.totalNum}}</td>
+              <td class="tr">{{productSellTypeList.totalWeight}}</td>
+              <td class="tr">{{productSellTypeList.totalGoldWeight}}</td>
+              <td class="tr">{{productSellTypeList.totalSoldPrice}}</td>
+              <td class="tr">{{productSellTypeList.totalWage}}</td>
+              <td class="tr">{{productSellTypeList.totalRealPrice}}</td>
             </tr>
           </template>
           <tr>
-            <td colspan="3">销售合计</td>
-            <td>{{productTypeList.totalNum}}</td>
-            <td>{{productTypeList.totalWeight}}</td>
-            <td>{{productTypeList.totalGoldWeight}}</td>
-            <td>{{productTypeList.totalSoldPrice}}</td>
-            <td>{{productTypeList.totalWage}}</td>
-            <td>{{productTypeList.totalRealPrice}}</td>
+            <td class="tm" colspan="3">销售合计</td>
+            <td class="tr">{{productTypeList.totalNum}}</td>
+            <td class="tr">{{productTypeList.totalWeight}}</td>
+            <td class="tr">{{productTypeList.totalGoldWeight}}</td>
+            <td class="tr">{{productTypeList.totalSoldPrice}}</td>
+            <td class="tr">{{productTypeList.totalWage}}</td>
+            <td class="tr">{{productTypeList.totalRealPrice}}</td>
           </tr>
         </template>
 
@@ -71,54 +71,54 @@
           <template v-if="productTypeList.productSellTypeList" v-for="(productSellTypeList,i) in productTypeList.productSellTypeList">
 
             <tr v-for="(item,index) in productSellTypeList.productTypeList" :key="index">
-              <td v-if="j == 0 && index == 0 && i == 0" :rowspan="sellLength(buyBackStorage.productTypeList)">回购</td>
-              <td v-if="index == 0" :rowspan="productSellTypeList.productTypeList.length">{{productSellTypeList.sellTypeName}}</td>
-              <td>{{item.className}}</td>
-              <td>{{item.totalNum}}</td>
-              <td>{{item.totalWeight}}</td>
-              <td>{{item.totalGoldWeight}}</td>
-              <td>{{item.totalSoldPrice}}</td>
-              <td>{{item.totalWage}}</td>
-              <td>{{item.totalActualPrice}}</td>
+              <td class="tm" v-if="j == 0 && index == 0 && i == 0" :rowspan="sellLength(buyBackStorage.productTypeList)">回购</td>
+              <td class="tm" v-if="index == 0" :rowspan="productSellTypeList.productTypeList.length">{{productSellTypeList.sellTypeName}}</td>
+              <td class="tl">{{item.className}}</td>
+              <td class="tr">{{item.totalNum}}</td>
+              <td class="tr">{{item.totalWeight}}</td>
+              <td class="tr">{{item.totalGoldWeight}}</td>
+              <td class="tr">{{item.totalSoldPrice}}</td>
+              <td class="tr">{{item.totalWage}}</td>
+              <td class="tr">{{item.totalActualPrice}}</td>
             </tr>
             <tr>
-              <td colspan="2">小计</td>
-              <td>{{productSellTypeList.totalNum}}</td>
-              <td>{{productSellTypeList.totalWeight}}</td>
-              <td>{{productSellTypeList.totalGoldWeight}}</td>
-              <td>{{productSellTypeList.totalSoldPrice}}</td>
-              <td>{{productSellTypeList.totalWage}}</td>
-              <td>{{productSellTypeList.totalActualPrice}}</td>
+              <td class="tm" colspan="2">小计</td>
+              <td class="tr">{{productSellTypeList.totalNum}}</td>
+              <td class="tr">{{productSellTypeList.totalWeight}}</td>
+              <td class="tr">{{productSellTypeList.totalGoldWeight}}</td>
+              <td class="tr">{{productSellTypeList.totalSoldPrice}}</td>
+              <td class="tr">{{productSellTypeList.totalWage}}</td>
+              <td class="tr">{{productSellTypeList.totalActualPrice}}</td>
             </tr>
           </template>
           <tr>
-            <td colspan="3">销售合计</td>
-            <td>{{productTypeList.totalNum}}</td>
-            <td>{{productTypeList.totalWeight}}</td>
-            <td>{{productTypeList.totalGoldWeight}}</td>
-            <td>{{productTypeList.totalSoldPrice}}</td>
-            <td>{{productTypeList.totalWage}}</td>
-            <td>{{productTypeList.totalActualPrice}}</td>
+            <td class="tm" colspan="3">销售合计</td>
+            <td class="tr">{{productTypeList.totalNum}}</td>
+            <td class="tr">{{productTypeList.totalWeight}}</td>
+            <td class="tr">{{productTypeList.totalGoldWeight}}</td>
+            <td class="tr">{{productTypeList.totalSoldPrice}}</td>
+            <td class="tr">{{productTypeList.totalWage}}</td>
+            <td class="tr">{{productTypeList.totalActualPrice}}</td>
           </tr>
         </template>
 
         <tr>
           <!-- <td>收款方式与金额</td> -->
-          <td colspan="2">刷卡(元)</td>
-          <td>现金(元)</td>
-          <td colspan="2">微信(元)</td>
-          <td colspan="2">支付宝(元)</td>
-          <td>其他(元)</td>
-          <td>实际收银(元)</td>
+          <td class="tm" colspan="2">刷卡(元)</td>
+          <td class="tm">现金(元)</td>
+          <td class="tm" colspan="2">微信(元)</td>
+          <td class="tm" colspan="2">支付宝(元)</td>
+          <td class="tm">其他(元)</td>
+          <td class="tm">实际收银(元)</td>
         </tr>
         <tr>
           <!-- <td>收款方式与金额</td> -->
-          <td colspan="2">{{cashierStatistics.slotCard || 0}}</td>
-          <td>{{cashierStatistics.cash || 0}}</td>
-          <td colspan="2">{{cashierStatistics.weixin || 0}}</td>
-          <td colspan="2">{{cashierStatistics.zhifubao || 0}}</td>
-          <td>{{cashierStatistics.other || 0}}</td>
-          <td>{{cashierStatistics.totalMoney || 0}}</td>
+          <td class="tr" colspan="2">{{cashierStatistics.slotCard || 0}}</td>
+          <td class="tr">{{cashierStatistics.cash || 0}}</td>
+          <td class="tr" colspan="2">{{cashierStatistics.weixin || 0}}</td>
+          <td class="tr" colspan="2">{{cashierStatistics.zhifubao || 0}}</td>
+          <td class="tr">{{cashierStatistics.other || 0}}</td>
+          <td class="tr">{{cashierStatistics.totalMoney || 0}}</td>
         </tr>
       </table>
     </div>
@@ -260,6 +260,15 @@
     font-size: 12px;
     border: 1px solid;
     line-height: 25px;
+    padding: 5px;
+  }
+  .tl{
+    text-align: left;
+  }
+  .tr{
+    text-align: right;
+  }
+  .tm{
     text-align: center;
   }
 
