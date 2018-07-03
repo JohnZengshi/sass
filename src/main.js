a2f5ace6267e241eb0ac36638a593df961fa0c12
wuxie <597111061@qq.com>
2018-07-03 16:14:25 +0800

    还换升级element-ui版本

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import elementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-default/index.css'
 import resource from 'vue-resource'
 import App from './App'
 import router from './router'
