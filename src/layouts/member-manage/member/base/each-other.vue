a18e2894f16ca9749626f2447dd4cd96a75b53a1
lqh <1040152089@qq.com>
2018-07-27 18:43:52 +0800

    优化

--- src/layouts/member-manage/member/base/each-other.vue
+++ src/layouts/member-manage/member/base/each-other.vue
@@ -202,7 +202,6 @@
         height: 100%;
         font-size: 14px;
         padding: 15px;
-        border-right: 1px solid #d6d6d6;
         vertical-align: top;
         text-align: center;
         display: inline-block;
