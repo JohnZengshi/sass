a4575179fe18c3d5f7b72531110213aad4cc700d
john <1351009316@qq.com>
2018-08-20 17:40:34 +0800

    管理标签弹窗列表按钮样式

---
+++ src/layouts/member-manage/push/components/dialogType/btnStyle.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // 管理标签弹窗列表按钮样式
+    signatureManageLsitBtnClass:{
+        defaultBtnClass:"btn-w50-h20 btn-f12 btn-backColor01 btn-color02 btn-noBorder",//默认按钮样式
+        generalBtnClass:"btn-w56-h18 btn-f12 btn-backColor04 btn-color03 btn-noBorder hover" //普通按钮样式
+    }
+}
